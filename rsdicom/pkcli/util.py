0a987c5c288a69f3ed6aa73ba5725ad067d4c801
e-carlin <evan@carlin.com>
2020-09-09 21:32:59 +0000

    ckp

--- rsdicom/pkcli/util.py
+++ rsdicom/pkcli/util.py
@@ -8,4 +8,23 @@ from __future__ import absolute_import, division, print_function
 
 
 def default_command():
+    compatible_chars = ['_', '-', '^', '+', '=', '!', '~']
+    incompatible_chars = [
+        '<',
+        '>',
+        ':',
+        '"',
+        '\'',
+        '/',
+        '\\',
+        '|',
+        '',
+        '',
+        '',
+        '',
+        '',
+    ]
     print('hello')
+    import pdfquery
+    pdf = pdfquery.PDFQuery('samplepdf1.pdf')
+    pdf.load()
