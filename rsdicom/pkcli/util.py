3f7f1e674ecea92f20890bfe6532c1d904042fd7
e-carlin <evan@carlin.com>
2020-09-09 22:25:29 +0000

    ckp

--- rsdicom/pkcli/util.py
+++ rsdicom/pkcli/util.py
@@ -7,6 +7,7 @@
 from __future__ import absolute_import, division, print_function
 
 from pykern.pkcollections import PKDict
+from pykern.pkdebug import pkdp
 
 def get_canonical_names():
     compatible_chars = ['_', '-', '^', '+', '=', '!', '~']
@@ -61,5 +62,15 @@ def get_canonical_names():
     print(pdf.pq('LTTextLineHorizontal:in_bbox("366.711, 298.95, 409.578, 307.95")').text())
 
 
-def nonconformant_names(file='/vagrant/ucla-140/ct/2.16.840.1.114362.1.11775105.22699029554.543997656.413.2799.dcm'):
+def nonconformant_names():
     import pydicom
+    f = [
+        # '/vagrant/ucla-140/ct/2.16.840.1.114362.1.11775105.22699029554.543997656.413.2799.dcm',
+        '/vagrant/ucla-140/rtst/2.16.840.1.114362.1.11775105.22699029554.543997669.1030.3153.dcm',
+        # '/vagrant/ucla-140/rtdose/2.16.840.1.114362.1.11775105.22699029554.543997671.684.3155.dcm ',
+        # '/vagrant/ucla-140/rtplan/2.16.840.1.114362.1.11775105.22699029554.543997669.1026.3146.dcm',
+    ]
+    for n in f:
+        r = pydicom.dcmread(n)[0x3006,0x0020]
+        for e in r:
+            pkdp('{}', e[0x3006,0x0026].value)
