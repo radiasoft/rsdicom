8151774b22d4450f2851a512d80c7cfd6e8ad64d
e-carlin <evan@carlin.com>
2020-09-18 20:53:24 +0000

    cleanup

--- rsdicom/pkcli/util.py
+++ rsdicom/pkcli/util.py
@@ -15,9 +15,12 @@ def default_command():
     ucla_140 = '/vagrant/ucla-140/rtst/2.16.840.1.114362.1.11775105.22699029554.543997669.1030.3153.dcm'
     coh_001 = '/vagrant/coh-001/rtst/RS.1.2.246.352.205.4827533863638583198.10720857379728702625.dcm'
 
-    # 0x3006,0x0020 is the location in the dicom where the ROI structure set is
-    # What is a structure set? => http://dicom.nema.org/dicom/2013/output/chtml/part03/sect_C.8.html#sect_C.8.8.5
-    # Where are locations of other interesting data? => https://dicom.innolitics.com/ciods/rt-dose/structure-set/30060020
-    r = pydicom.dcmread(ucla_140)[0x3006,0x0020]
+    # roi_structure_set_loc is the location in the dicom where the ROI structure set is.
+    # roi_name_loc is the location in the dicom where the ROI name is.
+    # Info about structure set and data contained within: http://dicom.nema.org/dicom/2013/output/chtml/part03/sect_C.8.html#sect_C.8.8.5
+    # Locations of other possibly interesting data: https://dicom.innolitics.com/ciods/rt-dose/structure-set/30060020
+    roi_structure_set_loc = (0x3006,0x0020)
+    r = pydicom.dcmread(ucla_140)[roi_structure_set_loc]
+    roi_name_loc = (0x3006,0x0026)
     for e in r:
-        pkdp('{}', e[0x3006,0x0026])
+        pkdp('{}', e[roi_name_loc])
